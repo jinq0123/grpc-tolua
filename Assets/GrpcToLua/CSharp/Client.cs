24f2efc73895304a51d850616cea28289cd5a734
Jin Qing <jq2019a@163.com>
2020-08-12 18:59:49 +0800

    Derive Client from grpc.ClientBase<Client>

--- Assets/GrpcToLua/CSharp/Client.cs
+++ Assets/GrpcToLua/CSharp/Client.cs
@@ -4,28 +4,52 @@ using grpc = global::Grpc.Core;
 
 namespace GrpcToLua
 {
-    public class Client
+    public class Client : grpc::ClientBase<Client>
     {
-        readonly grpc::ChannelBase channel;
         readonly string serviceName;
-
         MethodDictionary unaryMethods;
         MethodDictionary clientStreamingMethods;
         MethodDictionary serverStreamingMethods;
         MethodDictionary duplexStreamingMethods;
 
-        public Client(grpc::ChannelBase channel, string serviceName)
+        /// <summary>Creates a new client</summary>
+        /// <param name="channel">The channel to use to make remote calls.</param>
+        public Client(grpc::ChannelBase channel, string serviceName) : base(channel)
         {
-            this.channel = channel;
             this.serviceName = serviceName;
+            InitMethodDictionaries();
+        }
 
+        /// <summary>Creates a new client that uses a custom <c>CallInvoker</c>.</summary>
+        /// <param name="callInvoker">The callInvoker to use to make remote calls.</param>
+        public Client(grpc::CallInvoker callInvoker, string serviceName) : base(callInvoker)
+        {
+            this.serviceName = serviceName;
+            InitMethodDictionaries();
+        }
+
+        /// <summary>Protected parameterless constructor to allow creation of test doubles.</summary>
+        /* protected Client() : base()
+        {
+        }*/
+
+        /// <summary>Protected constructor to allow creation of configured clients.</summary>
+        /// <param name="configuration">The client configuration.</param>
+        protected Client(ClientBaseConfiguration configuration, string serviceName) : base(configuration)
+        {
+            this.serviceName = serviceName;
+            InitMethodDictionaries();
+        }
+
+        private void InitMethodDictionaries()
+        {
             unaryMethods = new MethodDictionary(serviceName, grpc.MethodType.Unary);
             clientStreamingMethods = new MethodDictionary(serviceName, grpc.MethodType.ClientStreaming);
             serverStreamingMethods = new MethodDictionary(serviceName, grpc.MethodType.ServerStreaming);
             duplexStreamingMethods = new MethodDictionary(serviceName, grpc.MethodType.DuplexStreaming);
         }
 
-        public UnaryCall UnaryCall(string methodName, LuaTable request)
+        public grpc::AsyncUnaryCall<LuaTable> UnaryCall(string methodName, LuaTable request)
         {
             Debug.LogFormat("Client.UnaryCall(methodNaame={0}, request={1})", methodName, request);
 
@@ -35,7 +59,7 @@ namespace GrpcToLua
             global::System.DateTime? deadline = null;
             global::System.Threading.CancellationToken cancellationToken = default(global::System.Threading.CancellationToken);
             var options = new grpc::CallOptions(headers, deadline, cancellationToken);
-            return callInvoker.AsyncUnaryCall(method, null, options, request);
+            return CallInvoker.AsyncUnaryCall(method, null, options, request);
         }
         
         public ServerStreamingCall ServerStreamingCall(string methodName, LuaTable request)
@@ -57,6 +81,12 @@ namespace GrpcToLua
             Debug.LogFormat("Client.DuplexStreamingCall(methodNaame={0}, request={1})", methodName, request);
             // TODO
             return new DuplexStreamingCall();
+        }
+
+        /// <summary>Creates a new instance of client from given <c>ClientBaseConfiguration</c>.</summary>
+        protected override Client NewInstance(ClientBaseConfiguration configuration)
+        {
+            return new Client(configuration, serviceName);
         }
     }  // class Client
 }  // namespace GrpcToLua
