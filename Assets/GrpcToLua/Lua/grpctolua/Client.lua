e367f9264f8969c735662a0fce1f989a508d7f1c
Jin Qing <jq2019a@163.com>
2020-08-12 14:34:43 +0800

    Fix construct()

--- Assets/GrpcToLua/Lua/grpctolua/Client.lua
+++ Assets/GrpcToLua/Lua/grpctolua/Client.lua
@@ -3,13 +3,13 @@
 local Client = {}
 Client.__index = Client
 
-local function construct(channel, service_name)
+local function construct(self, channel, service_name)
+    print(string.format("construct(self=%q(%s), channel=%q(%s), service_name=%q(%s))", self, type(self), channel, type(channel), service_name, type(service_name)))
     local clt = {}
     setmetatable(clt, Client)
 
     -- initialize
-    clt.channel = channel
-    clt.service_name = service_name
+    clt.client = GrpcToLua.Client.New(channel, service_name)
     return clt
 end
 
@@ -18,6 +18,8 @@ setmetatable(Client, {__call = construct})
 function Client:call(method_name, request)
     print(string.format("Client:call(method_name=%q, request=%q)", method_name, request))
     -- TODO
+    call = self.client:UnaryCall(method_name, request)
+    print(string.format("call: %q(%s)", call, type(call)))
     return { aaa = 123 }
 end
 
