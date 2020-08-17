2bec974a98fd7afc73ecaab2b2b0582df761e796
Jin Qing <jq2019a@163.com>
2020-08-17 21:56:29 +0800

    Client streaming and server streaming call

--- Assets/GrpcToLua/Lua/grpctolua/Client.lua
+++ Assets/GrpcToLua/Lua/grpctolua/Client.lua
@@ -21,8 +21,22 @@ setmetatable(Client, {__call = construct})
 
 function Client:call(method_name, request)
     print(string.format("Client:call(method_name=%q, request=%q)", method_name, request))
-    local methodInfo = assert(self.client:GetMethodInfo(method_name))
-    local request_data = pb.encode(methodInfo.input_type, request)
+    local info = assert(self.client:GetMethodInfo(method_name))
+    local request_data = pb.encode(info.input_type, request)
+    
+    local is_client_streaming = info.is_client_streaming
+    local is_server_streaming = info.is_server_streaming
+    if is_client_streaming and is_server_streaming then
+        return self:duplex_streaming_call(method_name, request_data, info.output_type)
+    elseif is_client_streaming then
+        return self:client_streaming_call(method_name, request_data, info.output_type)
+    elseif info.is_server_streaming then
+        return self:server_streaming_call(method_name, request_data, info.output_type)
+    end
+    self:unary_call(method_name, request_data, info.output_type)
+end  -- call()
+
+function Client:unary_call(method_name, request_data, output_type)
     local call = self.client:UnaryCall(method_name, request_data)
     await(call)
     print(string.format("call: %q(%s)", call, type(call)))
@@ -32,7 +46,7 @@ function Client:call(method_name, request)
     print(string.format("resp: %q(%s)", resp, type(resp)))
     local result = resp.Result
     print(string.format("result: %q(%s)", result, type(result)))
-    local respMsg = pb.decode(methodInfo.output_type, tolua.tolstring(result))
+    local respMsg = pb.decode(output_type, tolua.tolstring(result))
     return respMsg
 end
 
