e371afff45d97e608f37edfcbeb90b66b56772b5
Jin Qing <jq2019a@163.com>
2020-08-17 18:52:53 +0800

    Change TRequest and TResponse to byte[] because they require a reference type.

--- Assets/GrpcToLua/CSharp/MethodDictionary.cs
+++ Assets/GrpcToLua/CSharp/MethodDictionary.cs
@@ -6,10 +6,11 @@ using System;
 
 namespace GrpcToLua
 {
-    // grpc request and response both are string. Lua serializes and deserializes.
-    using Method = grpc::Method<string, string>;
+    // grpc TRequest and TResponse are both byte[] (must be reference type).
+    // Lua serializes and deserializes.
+    using Method = grpc::Method<byte[]/*TRequest*/, byte[]/*TResponse*/>;
     // using MethodDescriptor = gpr::MethodDescriptor;
-    using Marshaller = grpc::Marshaller<string>;
+    using Marshaller = grpc::Marshaller<byte[]>;
 
     // MethodDictionary maps from method name to Method instance
     public class MethodDictionary
@@ -18,10 +19,8 @@ namespace GrpcToLua
         readonly grpc::MethodType methodType;
         readonly Dictionary<string, Method> dict = new Dictionary<string, Method>();
 
-        static readonly Marshaller marshaller = grpc::Marshallers.Create(
-                System.Text.Encoding.ASCII.GetBytes,
-                System.Text.Encoding.ASCII.GetString
-            );
+        static readonly Marshaller marshaller =
+            grpc::Marshallers.Create((buf)=>buf, (buf)=>buf);
 
         public MethodDictionary(string serviceName, grpc::MethodType methodType)
         {
@@ -37,7 +36,8 @@ namespace GrpcToLua
                 return result;
             }
 
-            var newMethod = new Method(methodType, serviceName, methodName, marshaller, marshaller);
+            var newMethod = new Method(methodType, serviceName, methodName,
+                /*request*/marshaller, /*response*/marshaller);
             dict[methodName] = newMethod;
             return newMethod;
         }
