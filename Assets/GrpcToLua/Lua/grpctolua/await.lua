f876ba8aa7e74a2736e0a117e5d199e5cd4d391f
Jin Qing <jq2019a@163.com>
2020-09-01 08:02:07 +0800

    Add comment: await does not return the result, because in most cases the c# call to get the result is unnecessary.

--- Assets/GrpcToLua/Lua/grpctolua/await.lua
+++ Assets/GrpcToLua/Lua/grpctolua/await.lua
@@ -1,6 +1,7 @@
 -- lua coroutine simulate C# await
 -- awaitable is await-expressions
 -- https://docs.microsoft.com/zh-cn/dotnet/csharp/language-reference/language-specification/expressions#await-expressions
+-- await does not return the result, because in most cases the c# call to get the result is unnecessary.
 local function await(awaitable)
     awaiter = awaitable:GetAwaiter()
     -- tolua must have wait_until(), or merge from https://github.com/woshihuo12/tolua
